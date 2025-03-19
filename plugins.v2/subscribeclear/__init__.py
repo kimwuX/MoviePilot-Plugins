1d866688d33201e4c47affcd110bdc384ce2e3dc
kimwu2009 <kimwu2009@gmail.com>
2025-03-19 16:09:45 +0800

    subscribeclear v1.0.1

--- plugins.v2/subscribeclear/__init__.py
+++ plugins.v2/subscribeclear/__init__.py
@@ -18,7 +18,7 @@ class SubscribeClear(_PluginBase):
     # 插件图标
     plugin_icon = "Moviepilot_A.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "k0ala"
     # 作者主页
