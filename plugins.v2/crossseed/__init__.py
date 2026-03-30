912885d11ce3883ac233198cd3d173fb35c2abc3
kimwuX <kimwu2009@gmail.com>
2026-03-30 09:15:00 +0800

    crossseed v3.0.2.1

--- plugins.v2/crossseed/__init__.py
+++ plugins.v2/crossseed/__init__.py
@@ -180,7 +180,7 @@ class CrossSeed(_PluginBase):
     # 插件图标
     plugin_icon = "qingwa.png"
     # 插件版本
-    plugin_version = "3.0.1.6"
+    plugin_version = "3.0.2.1"
     # 插件作者
     plugin_author = "233@qingwa"
     # 作者主页
