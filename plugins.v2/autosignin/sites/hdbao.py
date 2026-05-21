b888f10185c699d1f08c94834185dc02c78e2775
kimwuX <kimwu2009@gmail.com>
2026-05-21 20:09:47 +0800

    autosignin v2.8.2.5

---
+++ plugins.v2/autosignin/sites/hdbao.py
@@ -0,0 +1,86 @@
+import re
+from typing import Tuple
+from urllib.parse import urljoin
+
+from ruamel.yaml import CommentedMap
+
+from app.core.config import settings
+from app.log import logger
+from app.plugins.autosignin.sites import _ISiteSigninHandler
+from app.utils.http import RequestUtils
+
+
+class HDBao(_ISiteSigninHandler):
+    """
+    红豆包签到
+    """
+
+    # 签到成功
+    # 这是您的第 <b>1</b> 次签到，已连续签到 <b>1</b> 天，本次签到获得 <b>10</b> 个魔力值。
+    _success_regex = [r'连续签到\s*\S*?\d+\S*?\s*天，本次签到获得']
+
+    @staticmethod
+    def get_netloc():
+        """
+        获取当前站点域名，可以是单个或者多个域名
+        """
+        return "hdbao.cc"
+
+    def signin(self, site_info: CommentedMap) -> Tuple[bool, str]:
+        """
+        执行签到操作
+        :param site_info: 站点信息，含有站点Url、站点Cookie、UA等信息
+        :return: 签到结果信息
+        """
+        site = site_info.get("name")
+        url = site_info.get("url")
+        site_cookie = site_info.get("cookie")
+        ua = site_info.get("ua")
+        proxy = site_info.get("proxy")
+        render = site_info.get("render")
+        timeout = site_info.get("timeout")
+
+        logger.info(f"开始以 {self.__class__.__name__} 模型签到 {site}")
+        signin_url = urljoin(url, "/attendance.php")
+
+        html_text = self.get_page_source(url=signin_url,
+                                         cookie=site_cookie,
+                                         ua=ua,
+                                         proxy=proxy,
+                                         render=render,
+                                         timeout=timeout)
+
+        if not html_text:
+            logger.warning(f"{site} 签到失败，请检查站点连通性")
+            return False, '签到失败，请检查站点连通性'
+
+        if "login.php" in html_text:
+            logger.warning(f"{site} 签到失败，Cookie已失效")
+            return False, '签到失败，Cookie已失效'
+
+        # 已签到
+        sign_status = self.sign_in_result(html_res=html_text,
+                                          regexs=self._success_regex)
+        if sign_status:
+            logger.info(f"{site} 今日已签到")
+            return True, '今日已签到'
+
+        # 签到
+        sign_res = RequestUtils(cookies=site_cookie,
+                                ua=ua,
+                                proxies=settings.PROXY if proxy else None,
+                                timeout=timeout
+                                ).post_res(url=signin_url)
+        if not sign_res or sign_res.status_code != 200:
+            logger.warning(f"{site} 签到失败，签到接口请求失败")
+            return False, '签到失败，签到接口请求失败'
+
+        # 判断是否签到成功
+        sign_status = self.sign_in_result(html_res=sign_res.text,
+                                          regexs=self._success_regex)
+        if sign_status:
+            logger.info(f"{site} 签到成功")
+            return True, '签到成功'
+
+        logger.warning(f"{site} 签到失败，接口返回：\n{sign_res.text}")
+        return False, '签到失败，请查看日志'
